357e52429bcb7dae60aa2a3a2c4c9d61e9bfbf9d
djhi12 <ido20002@byui.edu>
2024-01-31 18:20:01 +0800

    Initial commit

---
+++ src/app/pages/about-us.tsx
@@ -0,0 +1,12 @@
+// pages/about.tsx
+import React from 'react';
+
+const About = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>About Page</h1>
+    </main>
+  );
+};
+
+export default About;
